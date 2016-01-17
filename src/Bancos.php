e4571403548e1f2f4ef7b526cd8622213aeabd38
Ítalo Lelis de Vietro <italolelis@gmail.com>
2016-01-17 23:20:14 +0100

    New enum with Bancos

---
+++ src/Bancos.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Umbrella\YaBoleto;
+
+class Bancos
+{
+    // Lista de Bancos
+    const BANCO_BRASIL = 'Banco Brasil';
+    const SANTANDER = 'Santander';
+    const BRADESCO = 'Bradesco';
+    const CAIXA = 'Caixa Economica';
+    const ITAU = 'Itau';
+}
