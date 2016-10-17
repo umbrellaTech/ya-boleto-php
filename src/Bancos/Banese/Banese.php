811f354f47bfaef3af2ab807cacf30ca010d096c
Victor Felix <victor.gomes@voxtecnologia.com.br>
2016-10-17 17:23:30 -0300

    Iniciando implementacao do Bando do Estado de Sergipe - BANESE

---
+++ src/Bancos/Banese/Banese.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Umbrella\YaBoleto\Bancos\Banese;
+
+use Umbrella\YaBoleto\AbstractBanco;
+
+/**
+ * Classe do Banco do Estado de Sergipe - BANESE
+ *
+ * @author Victor Félix <victor.dreed@gmail.com>
+ * @package Umbrella\YaBoleto\Bancos\Banese
+ */
+class Banese extends AbstractBanco
+{
+    /**
+     * Inicializa uma nova instância da classe.
+     *
+     * @param string $agencia
+     * @param string $conta
+     */
+    public function __construct($agencia, $conta)
+    {
+        $numero = "047";
+        $nome = "Banco do Estado de Sergipe - BANESE";
+        parent::__construct($numero, $nome, $agencia, $conta);
+    }
+}
